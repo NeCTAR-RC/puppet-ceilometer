9de376478f38e65461306fffa732c15be1a0d4f8
Sam Morrison <sorrison@gmail.com>
2013-08-01 09:59:29 +1000

    Added initial manifests and templates
    Change-Id: I9d5c77fde7ceb788f55885b406a4877cc562d5c8

---
+++ manifests/api.pp
@@ -0,0 +1,28 @@
+class ceilometer::api inherits ceilometer {
+
+  package {'ceilometer-api':
+    ensure => installed,
+  }
+
+  service {'ceilometer-api':
+    ensure    => running,
+    subscribe => File['ceilometer-config'],
+    require   => Package['ceilometer-api'],
+  }
+
+  nagios::service {'ceilometer-api':
+    check_command => 'http_port!8777',
+    servicegroups => 'openstack-endpoints';
+  }
+
+  nagios::nrpe::service {'service_ceilometer_api':
+    check_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u ceilometer -a /usr/bin/ceilometer-api";
+  }
+
+  firewall {'100 ceilometer':
+    dport  => 8777,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+}
