ea00b58be26ac27df76e7770d6493ed65fd04597
Sam Morrison <sorrison@gmail.com>
2013-09-13 15:30:03 +1000

    Added options for secret and cells rpc dispatcher
    Change-Id: Ia58fd26face9fa06dac43ad7f41ef69b54d20a91

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,11 @@ class ceilometer($keystone_user,
                  $rabbit_user,
                  $rabbit_password,
                  $rabbit_virtual_host,
-                 $mongodb_host
+                 $disable_local_storage='False',
+                 $forward_metering_data='False',
+                 $forwarder_transport_url=false,
+                 $metering_secret,
+                 $database_connection='sqlite:////var/lib/ceilometer/ceilometer.sqlite',
 )
 {
 
