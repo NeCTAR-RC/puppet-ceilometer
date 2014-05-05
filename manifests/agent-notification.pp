9153b7ce8b26c76d199384e2d4c80ae465f3f884
Sam Morrison <sorrison@gmail.com>
2014-05-05 15:18:01 +1000

    Added new class for agent-notification
    Change-Id: I3490cb903e626669b007ea055cb790881d7ea9b0

---
+++ manifests/agent-notification.pp
@@ -0,0 +1,17 @@
+class ceilometer::agent-notification inherits ceilometer {
+
+  package {'ceilometer-agent-notification':
+    ensure => installed,
+  }
+
+  service {'ceilometer-agent-notification':
+    ensure    => running,
+    subscribe => File['ceilometer-config'],
+    require   => Package['ceilometer-agent-notification'],
+  }
+
+  nagios::nrpe::service {'service_ceilometer_agent_notification':
+    check_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u ceilometer -a /usr/bin/ceilometer-agent-notification";
+  }
+
+}
