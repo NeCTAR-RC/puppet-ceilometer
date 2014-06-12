9823f9efc840085662673347142931a0462bc66b
Sam Morrison <sorrison@gmail.com>
2014-06-13 09:43:43 +1000

    Added new classes to support alarms
    Change-Id: I66dcbfaddb04c8aa1f9e28014f169f12f2fd6269

---
+++ manifests/alarm-evaluator.pp
@@ -0,0 +1,17 @@
+class ceilometer::alarm-evaluator inherits ceilometer {
+
+  package {'ceilometer-alarm-evaluator':
+    ensure => installed,
+  }
+
+  service {'ceilometer-alarm-evaluator':
+    ensure    => running,
+    subscribe => File['ceilometer-config'],
+    require   => Package['ceilometer-alarm-evaluator'],
+  }
+
+  nagios::nrpe::service {'service_ceilometer_alarm_evaluator':
+    check_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u ceilometer -a /usr/bin/ceilometer-alarm-evaluator";
+  }
+
+}
