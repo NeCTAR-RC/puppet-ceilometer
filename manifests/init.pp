15a75721babcfe3c9b61111b63e3c7942c47c1b3
Sam Morrison <sorrison@gmail.com>
2015-06-05 11:50:42 +1000

    Ensure pymongo installed
    Change-Id: I372edd2500e19b5e855ba84f2e64a6f5d0607659

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,21 @@
-class ceilometer($keystone_user,
-                 $keystone_password,
-                 $keystone_region=false,
-                 $rabbit_hosts,
-                 $rabbit_user,
-                 $rabbit_password,
-                 $rabbit_virtual_host,
-                 $dispatchers='rpc',
-                 $disable_local_storage='False',
-                 $forward_metering_data='False',
-                 $forwarder_transport_url=false,
-                 $metering_secret,
-                 $database_connection='sqlite:////var/lib/ceilometer/ceilometer.sqlite',
-                 $logrotation='weekly',
-                 $db_ttl=7776000,
-                $agent_hostname=$::hostname,
+# Ceilometer base class
+class ceilometer(
+  $keystone_user,
+  $keystone_password,
+  $keystone_region=false,
+  $rabbit_hosts,
+  $rabbit_user,
+  $rabbit_password,
+  $rabbit_virtual_host,
+  $dispatchers='rpc',
+  $disable_local_storage='False',
+  $forward_metering_data='False',
+  $forwarder_transport_url=false,
+  $metering_secret,
+  $database_connection='sqlite:////var/lib/ceilometer/ceilometer.sqlite',
+  $logrotation='weekly',
+  $db_ttl=7776000,
+  $agent_hostname=$::hostname,
 )
 {
 
@@ -22,13 +24,13 @@ class ceilometer($keystone_user,
   $keystone_protocol = hiera('keystone::protocol')
   $keystone_service_tenant = hiera('keystone::service_tenant')
 
-  package {'ceilometer-common':
+  package {['ceilometer-common', 'python-pymongo']:
     ensure => installed
   }
 
   file {'ceilometer-config':
-    path    => '/etc/ceilometer/ceilometer.conf',
     ensure  => present,
+    path    => '/etc/ceilometer/ceilometer.conf',
     content => template("ceilometer/${openstack_version}/ceilometer.conf.erb"),
     require => Package['ceilometer-common'],
   }
@@ -36,7 +38,8 @@ class ceilometer($keystone_user,
   logrotate::rule { 'ceilometer':
     ensure  => present,
     path    => '/var/log/ceilometer/*.log',
-    options => [ 'rotate 4', $logrotation, 'missingok', 'compress', 'delaycompress', 'notifempty' ],
+    options => ['rotate 4', $logrotation, 'missingok',
+                'compress', 'delaycompress', 'notifempty'],
   }
 
   file { '/var/log/ceilometer':
