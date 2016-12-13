64f238fd1140c88f7ebc7bc5b9b9df5d6525827b
Jake Yip <jake.yip@unimelb.edu.au>
2016-12-14 10:50:45 +1100

    Remove hyphens in class name
    And move nagios checks to nectar module
    
    Change-Id: I8d8eea29a66331cd26a053343dbad76bb9ebaf3e

--- manifests/api/nagios-checks.pp
+++ manifests/api/nagios-checks.pp
@@ -1,11 +1,6 @@
-# Nagios checks for ceilometer API
+# Keep backwards compatability. Remove when everyone migrates to the new class
 class ceilometer::api::nagios-checks {
-  # These are checks that can be run by the nagios server.
-  nagios::command {
-    'check_ceilometer':
-      check_command => '/usr/lib/nagios/plugins/check_http -p \'$ARG1$\' -e 401 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
-    'check_ceilometer_ssl':
-      check_command => '/usr/lib/nagios/plugins/check_http --ssl -p \'$ARG1$\' -e 401 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
-  }
-}
+    include ::nectar::nagios::ceilometer
 
+    notify {'class ceilometer::api::nagios-checks is deprecated. Please use nectar::nagios::ceilometer': }
+}
