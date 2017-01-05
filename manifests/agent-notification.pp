5383c687edbd072aa62454536c645b7a3e8fbda4
Jake Yip <jake.yip@unimelb.edu.au>
2017-01-06 09:51:24 +1100

    Remove hyphens in class names
    Change-Id: Ifc29a1063f025a8f24e05486d19f2f212acbfa96

--- manifests/agent-notification.pp
+++ manifests/agent-notification.pp
@@ -1,23 +1,5 @@
 class ceilometer::agent-notification inherits ceilometer {
+  include ::ceilometer::agent_notification
 
-  package {'ceilometer-agent-notification':
-    ensure => installed,
-  }
-
-  service {'ceilometer-agent-notification':
-    ensure    => running,
-    subscribe => File['ceilometer-config'],
-    require   => Package['ceilometer-agent-notification'],
-  }
-
-  if $::ceilometer::openstack_version == 'juno' {
-    $procs = 2
-  } else {
-    $procs = 1
-  }
-
-  nagios::nrpe::service {'service_ceilometer_agent_notification':
-    check_command => "/usr/lib/nagios/plugins/check_procs -c ${procs}:${procs} -u ceilometer -a bin/ceilometer-agent-notification";
-  }
-
+  notify {'class ceilometer::agent-notification is deprecated. Please use ceilometer::agent_notification': }
 }
