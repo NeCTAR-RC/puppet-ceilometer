4702406072b85d4d9cb4b93176482265f94adba6
Sam Morrison <sorrison@gmail.com>
2018-05-03 10:16:11 +1000

    add support for objectstore polling namespace
    This is a quick fix with a better one being submitted upstream
    
    Change-Id: I8bee6ecffe0aa4f8c269d55f2036cfb2c65a229c

--- manifests/agent/polling.pp
+++ manifests/agent/polling.pp
@@ -90,6 +90,7 @@ class ceilometer::agent::polling (
   Boolean $central_namespace       = true,
   Boolean $compute_namespace       = true,
   Boolean $ipmi_namespace          = true,
+  Boolean $objectstore_namespace   = false,
   $instance_discovery_method       = $facts['os_service_default'],
   $resource_update_interval        = $facts['os_service_default'],
   $resource_cache_expiry           = $facts['os_service_default'],
@@ -191,10 +192,17 @@ class ceilometer::agent::polling (
     }
   }
 
+  if $objectstore_namespace {
+    $objectstore_namespace_name = 'objectstore'
+  } else {
+    $objectstore_namespace_name = ''
+  }
+
   $namespaces_real = delete_undef_values([
     $central_namespace_name,
     $compute_namespace_name,
-    $ipmi_namespace_name
+    $ipmi_namespace_name,
+    $objectstore_namespace_name
   ])
 
   if empty($namespaces_real) or $separate_services {
