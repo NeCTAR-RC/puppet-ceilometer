23013402e1d0e7dfa5435a8c6d20d40de31495a2
Sam Morrison <sorrison@gmail.com>
2018-05-03 10:16:11 +1000

    add support for objectstore polling namespace
    This is a quick fix with a better one being submitted upstream
    
    Change-Id: I8bee6ecffe0aa4f8c269d55f2036cfb2c65a229c
    (cherry picked from commit 0a9639896582a66f31a8a58991a52a301f5e8a28)
    (cherry picked from commit 9bc1918ac155d393ad56c107eb1fed760dd7690a)

--- manifests/agent/polling.pp
+++ manifests/agent/polling.pp
@@ -38,6 +38,10 @@
 #   (Optional) Use ipmi namespace for polling agent.
 #   Defaults to true.
 #
+# [*objectstore_namespace*]
+#   (Optional) Use objectstore namespace for polling agent.
+#   Defaults to false.
+#
 # [*instance_discovery_method*]
 #   (Optional) method to discovery instances running on compute node
 #   Defaults to $facts['os_service_default']
@@ -132,6 +136,7 @@ class ceilometer::agent::polling (
   Boolean $central_namespace              = true,
   Boolean $compute_namespace              = true,
   Boolean $ipmi_namespace                 = true,
+  Boolean $objectstore_namespace          = false,
   $instance_discovery_method              = $facts['os_service_default'],
   $resource_update_interval               = $facts['os_service_default'],
   $resource_cache_expiry                  = $facts['os_service_default'],
@@ -240,10 +245,17 @@ class ceilometer::agent::polling (
     }
   }
 
+  if $objectstore_namespace {
+    $objectstore_namespace_name = 'objectstore'
+  } else {
+    $objectstore_namespace_name = ''
+  }
+
   $namespaces_real = delete_undef_values([
     $central_namespace_name,
     $compute_namespace_name,
     $ipmi_namespace_name,
+    $objectstore_namespace_name,
   ])
 
   if empty($namespaces_real) or $separate_services {
